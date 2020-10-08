96cf04e35876df3b02290dcc0d0a468400776f08
ArturDziadosz <artur.dziadosz1@gmail.com>
2020-10-08 23:05:46 +0200

    Added displaing delete btn on hover li element

--- src/components/TD/TDElement.js
+++ src/components/TD/TDElement.js
@@ -4,21 +4,40 @@ import './TDElement.scss';
 
 class TDElement extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false
+    }
+  }
+
   deleteTask = e => {
+    if (this.state.checked === false) {
+      return false;
+    } else {
     const index = e.target.getAttribute("data-key");
     this.props.handleAtParent(index);
+    }
   }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.checked
+    })
+  };
 
   render() {
     return (
       <>
-        <li>
-            {this.props.task.text}
+        <li className={this.state.checked ? "tdList__element tdList__element--checked" : "tdList__element"} >
+            <p>{this.props.task.text}</p>
+            <input type={"checkbox"} name={"checked"} onClick={e => this.handleChange(e)}/>
             <button type={"button"}
                     data-key={this.props.index}
-                    onClick={e => this.deleteTask(e)}        
+                    onClick={e => this.deleteTask(e)}
+                    className={"tdList__element__deleteBtn"}        
             >
-                Delete
+                <i className="fas fa-trash-alt" data-key={this.props.index} />
             </button>
         </li>
       </>
